65965bfd127f94f4b9450dd502dd0a2aee28c06d
Jorge Ulises Useche Cuellar <naturalmentejorge@gmail.com>
2019-03-17 09:56:56 -0500

    Remove customizations

--- config/entrypoint.sh
+++ config/entrypoint.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 # export DISPLAY=:99
 /usr/bin/Xvfb :99 -screen 0 1024x768x24 -ac +extension GLX +render -noreset &
-echo "Open http://localhost:8080/cgi-bin/qgis_mapserv.fcgi?SERVICE=WMS&VERSION=1.3.0&REQUEST=GetCapabilities"
+# cp "/home/qgis/projects/madera/ESRI US Forestry 2 Regular/ESRI US Forestry 2 Regular.ttf" /usr/share/fonts/
+# fc-cache -f -v
 /usr/sbin/apachectl -D FOREGROUND &
+echo "Open http://localhost:8080/cgi-bin/qgis_mapserv.fcgi?SERVICE=WMS&VERSION=1.3.0&REQUEST=GetCapabilities"
 touch /var/log/qgis/qgisserver.log
 tail -f /var/log/qgis/qgisserver.log
